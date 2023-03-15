60782dca5ca8ab23fd6338987b72ecd583a5712b
Matt Born <mattborn@users.noreply.github.com>
2023-03-15 11:20:37 -0700

    The saga begins: a GPT text adventure experiment

---
+++ scripts.js
@@ -0,0 +1,111 @@
+let history = [
+  {
+    role: 'system',
+    content:
+      'You are a friendly, light-hearted local guide telling me about what is around me.',
+  },
+]
+
+const g = document.getElementById.bind(document)
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoibWF0dGJvcm4iLCJhIjoiY2w1Ym0wbHZwMDh3eTNlbnh1aW51cm0ydyJ9.Z5h4Vkk8zqjf6JydrOGXGA'
+
+const heyI = async (messages) => {
+  const response = await fetch(
+    'https://us-central1-samantha-374622.cloudfunctions.net/turbo',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(messages),
+    },
+  )
+  return response.text()
+}
+
+// ok we are going to do a bunch of things in series
+
+const enrichPosition = (c) => {
+  history.push({
+    role: 'user',
+    content: `Please tell me the state, city, local area or neighborhood name, and approximate address for the following coordinates: ${c}.`,
+  })
+  heyI(history).then((text) => {
+    history.push({
+      role: 'assistant',
+      content: text,
+    })
+    g('position').textContent = text
+    getVenues(c)
+  })
+}
+
+const getVenues = (c) => {
+  history.push({
+    role: 'user',
+    content: `Please tell me three closest event venues and their distances in miles from the following coordinates: ${c}.`,
+  })
+  heyI(history).then((text) => {
+    history.push({
+      role: 'assistant',
+      content: text,
+    })
+    g('venue').textContent = text
+    makeDay(c)
+  })
+}
+
+const makeDay = (c) => {
+  history.push({
+    role: 'user',
+    content: `Ok, pick one of those three venues for me and plan a simple schedule for me with 2–4 other family-friendly restaurants or things to do near that venue.`,
+  })
+  heyI(history).then((text) => {
+    history.push({
+      role: 'assistant',
+      content: text,
+    })
+    g('schedule').textContent = text
+    console.log(history)
+  })
+}
+
+const data = [
+  {
+    name: 'Milwaukee Public Museum',
+    image:
+      'https://www.milwaukeemag.com/wp-content/uploads/2022/07/05-Commons_05-scaled.jpg',
+    latitude: 43.040613,
+    longitude: -87.920495,
+  },
+]
+
+let coords = [-87.9, 43.04]
+
+navigator.geolocation.getCurrentPosition(
+  (p) => {
+    coords = [p.coords.longitude, p.coords.latitude]
+    renderMap()
+  },
+  (e) => renderMap(),
+)
+
+function renderMap() {
+  enrichPosition(coords)
+  // console.log(coords)
+  const map = new mapboxgl.Map({
+    center: coords,
+    container: 'map1',
+    style: 'mapbox://styles/mapbox/streets-v12',
+    zoom: 11,
+  })
+  map.addControl(new mapboxgl.NavigationControl())
+  const marker1 = new mapboxgl.Marker().setLngLat(coords).addTo(map)
+  data.forEach((item) => {
+    const marker = new mapboxgl.Marker()
+      .setLngLat([item.longitude, item.latitude])
+      .addTo(map)
+  })
+}
